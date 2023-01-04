f7603655d9baea2dc97c8036f00f357763567a5a
jyotsanashyama <63206848+jyotsanashyama@users.noreply.github.com>
2023-01-04 18:43:33 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,9 +1,9 @@
 #Program to find the Cube of elements up to a given number in python.
-
+#create a function
 def cube(n):
     cubes={}
-    for i in range(1,n+1):
+    for i in range(1,n+1):  # use for loop
         cubes[i]=i**3
     return cubes
-x=int(input("Enter the number : "))
+x=int(input("Enter the number : ")) # x is any variable that takes input from user
 print(cube(x))
